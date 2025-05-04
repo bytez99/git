3a909a905a265337c42db97a32d3910beee829d8
derice <bytez9001@gmail.com>
2025-05-04 06:21:19 +0100

    TreeEntry.java
    Updated to private variables with getters/setters

--- src/main/java/TreeEntry.java
+++ src/main/java/TreeEntry.java
@@ -1,9 +1,12 @@
 public class TreeEntry {
-    int headerSize;
-    String headerType;
-    String mode;
-    String name;
-    byte[] sha;
+    private int headerSize;
+    private String headerType;
+    private String mode;
+
+
+
+    private String name;
+    private byte[] sha;
 
     public TreeEntry(String headerType, int headerSize, String mode, String name, byte[] sha) {
         this.headerType = headerType;
@@ -19,5 +22,45 @@ public class TreeEntry {
             hexString.append(String.format("%02X", b));
         }
         return hexString.toString();
+    }
+
+    public byte[] getSha() {
+        return sha;
+    }
+
+    public void setSha(byte[] sha) {
+        this.sha = sha;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getHeaderType() {
+        return headerType;
+    }
+
+    public void setHeaderType(String headerType) {
+        this.headerType = headerType;
+    }
+
+    public int getHeaderSize() {
+        return headerSize;
+    }
+
+    public void setHeaderSize(int headerSize) {
+        this.headerSize = headerSize;
     }
 }
